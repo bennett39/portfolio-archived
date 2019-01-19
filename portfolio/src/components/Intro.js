d0270824ea936d513eb4e36e8e2e86e40af2f34d
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-19 18:01:24 -0500

    Implement starter site

---
+++ portfolio/src/components/Intro.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+  render() {
+    return (
+      <div className="intro">
+            <h1 className="display-1">Bennett Garner</h1> 
+            <span className="pl-2 lead text-muted text-right">
+              Django/NodeJS Backend Developer
+            </span>
+      </div>
+    );
+  }
+}
+
+export default Intro;
--- portfolio/src/components/Site.js
+++ portfolio/src/components/Site.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
+import Intro from './Intro.js';
 
 class Site extends Component {
   render() {
     return (
-      <div className="site">
-        <h1>Bennett Garner</h1>
-        <span>Django/NodeJS Backend Developer</span>
-        <p>This is a paragraph with <strong>bold</strong>.</p>
-        <p>A paragraph with <em>italics</em>.</p>
-        <code>A code snippet code.snip()</code>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <h4>Heading 4</h4>
+      <div className="site container">
+        <Intro />
       </div>
     );
   }
