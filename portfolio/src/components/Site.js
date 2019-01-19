8fb7c20716867b821762345edee1341486568ada
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-19 18:58:14 -0500

    Delete unwanted changes

--- portfolio/src/components/Site.js
+++ portfolio/src/components/Site.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Intro from './Intro.js';
-import TicTacToe from './TicTacToe.js';
 
 class Site extends Component {
   render() {
     return (
       <div className="site container">
         <Intro />
-        <TicTacToe />
       </div>
     );
   }
